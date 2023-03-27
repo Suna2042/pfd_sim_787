fabead3ae33b83c7477a3915e569eaf26a7d4416
Suna2042 <skylink2042@gmail.com>
2023-03-27 18:27:27 +0900

    add

--- setup.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cx_Freeze import setup, Executable
-
-base=None
-
-exe=Executable(script='main.py',base=base)
-
-setup(name='pfd',version='1.0',description='converter',executables=[exe])
